4cfdfef084f9d393b750c4c63623924341638b02
Luis Reyes Fallas <reyesfallas02@gmail.com>
2026-09-16 13:12:58 -0600

    Mejora diseño visual del login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,6 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Login.css';
-import { useEffect} from 'react';
 
 function Login() {
   const navigate = useNavigate();
